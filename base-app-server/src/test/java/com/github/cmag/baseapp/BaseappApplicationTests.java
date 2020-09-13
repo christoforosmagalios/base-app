b94ed6fd1835025f7f7204f4e07d9f2abf90de1e
Christoforos Magalios <xristoforos.magalios@gmail.com>
2020-09-13 12:04:14 +0300

    Initial commit.

---
+++ base-app-server/src/test/java/com/github/cmag/baseapp/BaseappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.cmag.baseapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
