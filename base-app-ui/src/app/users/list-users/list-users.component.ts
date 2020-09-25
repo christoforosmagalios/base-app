36a5c9bf820b0d8e62a429440592f831ce6989c7
Christoforos Magalios <xristoforos.magalios@gmail.com>
2020-09-25 20:53:17 +0300

    Display users, Pagination

--- base-app-ui/src/app/users/list-users/list-users.component.ts
+++ base-app-ui/src/app/users/list-users/list-users.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PageDTO } from '../../dto/page-dto';
+import { UserDTO } from '../../dto/user-dto';
 import { UserService } from '../user.service';
 
 @Component({
@@ -8,9 +10,31 @@ import { UserService } from '../user.service';
 })
 export class ListUsersComponent implements OnInit {
 
+  // The list of users.
+  users = [];
+  // Active page.
+  page = 1;
+  // Total number of users in the Database.
+  collectionSize: number;
+  // Users per page.
+  size: number = 10;
+
   constructor(private userService: UserService) { }
 
   ngOnInit() {
+    // Find all users in order to display them.
+    this.findAll();
+  }
+
+  /**
+   * Find all the users.
+   */
+  findAll() {
+    this.userService.findAll(this.size, (this.page - 1), "createdOn", "desc")
+    .subscribe((page: PageDTO<UserDTO>) => {
+      this.users = page.content;
+      this.collectionSize = page.totalElements;
+    });
   }
 
 }
--- base-app-ui/src/app/users/users.module.ts
+++ base-app-ui/src/app/users/users.module.ts
@@ -4,6 +4,7 @@ import { ListUsersComponent } from './list-users/list-users.component';
 import { UsersRoutingModule } from './users.routing.module';
 import { UserComponent } from './user/user.component';
 import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 
@@ -12,7 +13,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     UsersRoutingModule,
-    FormsModule
+    FormsModule,
+    NgbModule
   ]
 })
 export class UsersModule { }
